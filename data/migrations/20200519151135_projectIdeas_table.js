6b9defe6653809f186acf6a66577fbe34fa0eee4
Your Name <you@example.com>
2020-05-19 15:21:33 -0700

    remigrated

---
+++ data/migrations/20200519151135_projectIdeas_table.js
@@ -0,0 +1,17 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('projectIdeas', tbl => {
+        tbl.increments();
+        tbl.string('project_name');
+        tbl.string('category');
+        tbl.string('target_user');
+        tbl.string('impact');
+        tbl.string('human_resources');
+        tbl.integer('join_count');
+        tbl.string('description');
+        tbl.integer('upvote')
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('projectIdeas')
+};
--- data/migrations/20200519151210_projectArchive_table.js
+++ data/migrations/20200519151210_projectArchive_table.js
@@ -1,16 +1,18 @@
 
 exports.up = function(knex) {
-    return knex.schema.createTable('projectIdeas', tbl => {
+    return knex.schema.createTable('projectArchive', tbl => {
         tbl.increments();
         tbl.string('project_name');
         tbl.string('category');
         tbl.string('target_user');
         tbl.string('impact');
         tbl.string('human_resources');
-        tbl.integer('join_count')
+        tbl.integer('join_count');
+        tbl.string('description');
+        tbl.integer('upvote')
     })
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('projectIdeas')
+    return knex.schema.dropTableIfExists('projectArchive')
 };
