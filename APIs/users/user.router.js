23ec133a0012db93c97c0236d4bbb2f6969035aa
Your Name <you@example.com>
2020-06-02 09:49:16 -0700

    try to work around login route

--- APIs/users/user.router.js
+++ APIs/users/user.router.js
@@ -31,7 +31,7 @@ router.post('/', (req,res) => {
 router.post('/login', (req, res) => {
     let {email, password} = req.body;
 
-    usersModel.findBy(email)
+    usersModel.findBy({email})
               .first()
               .then(user => {
                   if (user && bcrypt.compareSync(password, user.password)){
