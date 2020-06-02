8db33f98ee7cd0be2634379479e6c03aef9582ac
Your Name <you@example.com>
2020-06-02 11:36:22 -0700

    added return id in login

--- APIs/users/user.router.js
+++ APIs/users/user.router.js
@@ -37,7 +37,9 @@ router.post('/login', (req, res) => {
               .then(user => {
                   if (user && bcrypt.compareSync(password, user.password)){
                       req.session.user = user;
-                      res.status(200).json({message: `Welcome ${user.email}`});
+                      res.status(200).json({
+                          message: `Welcome ${user.email}`,
+                          id: user.id});
                   }
                   else {
                       res.status(401).json({message: 'Invalid Credentials'});
