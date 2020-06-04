cc972ed881c680a2e39fcffc8053acabc6cb152c
Your Name <you@example.com>
2020-06-04 15:07:13 -0700

    POST and GET requests for category work

--- APIs/category/category.model.js
+++ APIs/category/category.model.js
@@ -1,14 +1,24 @@
 const db = require('../../data/dbConfig');
 
 function getProjectsPerCate(cateId){
-    console.log('cateId', cateId)
     return db("category as c")
             .where("c.id", cateId)
             .join("projectIdeas as p", "p.category_id", "c.id" )           
 }
 
+function getCates(){
+    return db("category")
+}
+
+function addCate(cate){
+    return db("category")
+            .insert(cate)
+            .then(ids => ({id: ids[0]}))
+}
 
 
 module.exports = {
-    getProjectsPerCate
+    getProjectsPerCate,
+    getCates,
+    addCate
 }
--- APIs/category/category.router.js
+++ APIs/category/category.router.js
@@ -2,6 +2,29 @@ const router = require('express').Router();
 const cateModel = require('./category.model');
 
 
+//GET all categories
+router.get('/', (req, res) => {
+    cateModel.getCates()
+             .then(cates => {
+                 res.status(200).json(cates)
+             })
+             .catch(err => {
+                 res.status(500).json(err.message)
+             })
+})
+
+//POST a category
+router.post('/', (req,res) => {
+    const cate = req.body
+    cateModel.addCate(cate)
+             .then(id => {
+                res.status(200).json(id)
+             })
+             .catch(err => {
+                res.status(500).json(err.message)
+             })
+})
+
 //GET projects per cate
 router.get('/:id', (req,res) => {
     const id = req.params.id
@@ -14,6 +37,5 @@ router.get('/:id', (req,res) => {
             })
 
 })
-
 
 module.exports = router;
