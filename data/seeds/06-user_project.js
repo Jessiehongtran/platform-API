4eef294e536f5e5e1e80338693bc74d256b41860
Your Name <you@example.com>
2020-06-09 15:59:03 -0700

    seeded successfully user table and user_project table

--- data/seeds/06-user_project.js
+++ data/seeds/06-user_project.js
@@ -13,91 +13,91 @@ exports.seed = function(knex) {
           is_member: false
         },
         {
-          id: 1, 
+          id: 2, 
           user_id: 1,
           project_id: 2,
           is_host: true,
           is_member: false
         },
         {
-          id: 1, 
+          id: 3, 
           user_id: 1,
           project_id: 3,
           is_host: true,
           is_member: false
         },
         {
-          id: 1, 
+          id: 4, 
           user_id: 1,
           project_id: 4,
           is_host: true,
           is_member: false
         },
         {
-          id: 1, 
+          id: 5, 
           user_id: 1,
           project_id: 5,
           is_host: true,
           is_member: false
         },
         {
-          id: 1, 
+          id: 6, 
           user_id: 1,
           project_id: 6,
           is_host: true,
           is_member: false
         },
         {
-          id: 1, 
+          id: 7, 
           user_id: 1,
           project_id: 7,
           is_host: true,
           is_member: false
         },
         {
-          id: 1, 
+          id: 8, 
           user_id: 1,
           project_id: 8,
           is_host: true,
           is_member: false
         },
         {
-          id: 1, 
+          id: 9, 
           user_id: 1,
           project_id: 9,
           is_host: true,
           is_member: false
         },
         {
-          id: 1, 
+          id: 10, 
           user_id: 1,
           project_id: 10,
           is_host: true,
           is_member: false
         },
         {
-          id: 1, 
+          id: 11, 
           user_id: 1,
           project_id: 11,
           is_host: true,
           is_member: false
         },
         {
-          id: 1, 
+          id: 12, 
           user_id: 1,
           project_id: 12,
           is_host: true,
           is_member: false
         },
         {
-          id: 1, 
+          id: 13, 
           user_id: 1,
           project_id: 13,
           is_host: true,
           is_member: false
         },
         {
-          id: 1, 
+          id: 14, 
           user_id: 1,
           project_id: 14,
           is_host: true,
