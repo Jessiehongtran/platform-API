d38aed1210bf8d0309178421c539dc2a582e218b
Hong Tran <jessietran@fb.com>
2021-10-06 07:30:53 -0700

    added subcomment table

---
+++ data/migrations/20211006072451_subcomment_table.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('subcomment', tbl => {
+      tbl.increments()
+      tbl.integer('subcomment_ID')
+         .unsigned()
+         .references('id')
+         .inTable('comment_user')
+         .onDelete('CASCADE')
+         .onUpdate('CASCADE')
+      tbl.integer('user_ID')
+         .unsigned()
+         .references('id')
+         .inTable('users')
+         .onDelete('CASCADE')
+         .onUpdate('CASCADE')
+      tbl.string('subcomment')
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('subcomment')
+};
