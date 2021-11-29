5eede96156169b42fd2c334142aa62d2a8cb8df9
Hong Tran <jessietran@fb.com>
2021-11-29 15:04:16 -0800

    added config for ssl

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,10 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    },
     migrations: {
       directory: './data/migrations'
     },
